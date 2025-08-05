de180bf815d8768f19b999b7b59e39d5f5be29be
김원우 <25_13@bssm.hs.kr>
2025-08-05 18:22:39 +0900

    헬퍼 삭제

---
+++ utility_vite/src/ex02.js
@@ -0,0 +1,76 @@
+import * as THREE from "three";
+import { ThreeMFLoader } from "three/examples/jsm/Addons.js";
+import Stats  from "stats.js";
+
+// ----- 주제: 초당 프레임 수 보기
+
+export default function example() {
+  // Renderer
+  const canvas = document.querySelector("#three-canvas");
+  const renderer = new THREE.WebGLRenderer({
+    canvas,
+    antialias: true,
+  });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
+
+  // Scene
+  const scene = new THREE.Scene();
+
+  // Camera
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+  camera.position.x = 1;
+  camera.position.y = 3;
+  camera.position.z = 0;
+  scene.add(camera);
+
+  //light
+  const AmbientLight = new THREE.AmbientLight();
+  scene.add(AmbientLight);
+  const directionalLight = new THREE.DirectionalLight("white", 0.5);
+  scene.add(THREE.AmbientLight);
+  directionalLight.position.x = 1;
+  directionalLight.position.z = 2;
+  scene.add(directionalLight);
+
+  // Mesh
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const material = new THREE.MeshStandardMaterial({
+    color: "seagreen",
+  });
+  const mesh = new THREE.Mesh(geometry, material);
+  mesh.position.x = 2;
+  scene.add(mesh);
+
+  //mesh 보게 하기
+  camera.lookAt(mesh.position);
+
+  // 그리기
+  const clock = new THREE.Clock();
+
+  function draw() {
+    const time = clock.getElapsedTime();
+
+    mesh.rotation.y = time;
+
+    renderer.render(scene, camera);
+    window.requestAnimationFrame(draw);
+  }
+
+  function setSize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.render(scene, camera);
+  }
+
+  // 이벤트
+  window.addEventListener("resize", setSize);
+
+  draw();
+}
